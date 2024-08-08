de9a39aa630b20b697be1555ec714ff484204f3f
Sujit K M <kmsujit@gmail.com>
2024-08-08 15:18:16 +0530

    minor typos in type definition
    minor typos in type definition

--- src/results.h
+++ src/results.h
@@ -4,13 +4,13 @@
 template<class P, class T>  
 class Results{
     P results_object;
-    T[] results_tuples;
+    T results_tuples [];
 
     public: void set_param_object(P results_object){
         this->results_object = results_object;
     }    
 
-    public: void set_param_tuple(T[] results_tuples){
+    public: void set_param_tuple(T results_tuples []){
         this->results_tuples = results_tuples;
     }
 };
